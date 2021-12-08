5cfd89e790e96ed8aea2bfb655d2404d5bc9fd61
gourav-08 <58393079+gourav-08@users.noreply.github.com>
2021-12-08 21:40:00 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+import sys
+import pickle
+import json
+import requests
+import time
+from pyspark import SparkConf,SparkContext
+from pyspark.sql.functions import col
+from pyspark.sql import SparkSession,SQLContext,Row
+from pyspark.streaming import StreamingContext
+from pyspark.sql.types import StructType,StructField,StringType
+from preprocess import pre_process
+from models import PAClassifier
+
+my_schema = StructType([StructField('id', StringType(), False), StructField('Tweet', StringType(), False)])
+
+
+config = SparkConf()
+config.setAppName("Sentiment")
+sc = SparkContext(conf=config)
+
+
+def create_dataframe(rdd):
+	sp = SparkContext.getOrCreate()
+	sqlContext = SQLContext(sp)
+	collection = rdd.collect()
+	
+    records = []
+	
+	for i in collection:
+		for j in json.loads(i).values():
+			records.append(j)
+	
+    
+	rows = map(lambda x: Row(**x),records) 
+	Dataframe = sqlContext.createDataFrame(rows,my_schema)
+	if Dataframe.count() >0:
+		Dataframe = preprocess(Dataframe)
+		Model1 = PAClassifier(Dataframe)
+		pickle.dump(Model1,open("PAC.pickle",'wb'))
+
+ssc = StreamingContext(sc,5)
+
+ssc.checkpoint("checkpoint_Sentiment")
+
+dataStream = ssc.socketTextStream("localhost",6100)
+
+dataStream.foreachRDD(create_dataframe)
+
+ssc.start()    
+ssc.awaitTermination()
+ssc.stop()
+
