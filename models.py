69c6b8f45eeb5953b92462f2ccfdcab0d168329e
Likhith-Reddy <52623965+Likhith-Reddy@users.noreply.github.com>
2021-12-09 11:17:50 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -3,9 +3,9 @@ import numpy as np
 
 def PAClassifier(df):
 	classifier = PassiveAggressiveClassifier()
-	sentiment = np.array(df.select("Sentiment id").collect())
+	sentiment = np.array(df.select("Sentiment").collect())
 	features = np.array(df.select("features").collect())
 	new_sentiment = np.squeeze(sentiment)
 	new_feature = np.squeeze(features)
 	classifier.partial_fit(new_feature,new_sentiment,classes = np.unique(new_sentiment))
-	return classfier
+	return classifier
